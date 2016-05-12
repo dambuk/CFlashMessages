44f4dd7d28cbb262a8ea702e5add347e33075ea9
dambuk <bukowski.damian81@gmail.com>
2016-05-12 17:45:45 +0200

    test

---
+++ test/CFlashMessages/CFlashMessagesTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace dambuk\CFlashMessages;
+/**
+ * HTML Form elements.
+ *
+ */
+class CFlashMessagesTest extends \PHPUnit_Framework_TestCase
+{
+public function testNewMessage() {
+$el = new \dambuk\CFlashMessages\CFlashMessages();
+        $el->addMessage('content', 'type');
+        $res = $el->getFlashMessages();
+        $exp = "<div class='type'><p>content</p></div>";
+        $this->assertEquals($res, $exp, "Missmatch");
+}
+public function testShowMsg() {
+ 
+$el = new \dambuk\CFlashMessages\CFlashMessages();
+        $res = $el->getFlashMessages();
+        $exp = null;
+        $this->assertEquals($res, $exp, "Missmatch");
+}
+}
+?>
